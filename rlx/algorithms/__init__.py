3db4d406088cea93260c5cd92e06c28e630b57cd
Noah Farr <login@noahfarr.com>
2026-06-30 15:06:48 +0200

    feat: add TD3 algorithm with compiled updates
    Add Twin-Delayed DDPG (TD3) algorithm and example, plus a shared
    soft_update target-network helper in rlx.utils. The critic and
    (delayed) actor updates are wrapped in mx.compile with network/optimizer
    state captured via inputs/outputs.

--- rlx/algorithms/__init__.py
+++ rlx/algorithms/__init__.py
@@ -3,6 +3,7 @@ from rlx.algorithms.reinforce import REINFORCE, REINFORCEConfig
 from rlx.algorithms.a2c import A2C, A2CConfig
 from rlx.algorithms.ppo import PPO, PPOConfig
 from rlx.algorithms.sac import SAC, SACConfig
+from rlx.algorithms.td3 import TD3, TD3Config
 
 __all__ = [
     "DQN",
@@ -15,4 +16,6 @@ __all__ = [
     "PPOConfig",
     "SAC",
     "SACConfig",
+    "TD3",
+    "TD3Config",
 ]
